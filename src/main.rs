8ccbf44a99bb64c9e96e17424b2bb2b23506efcc
Sergio Castaño Sánchez <sergiocastanosanchez@icloud.com>
2026-08-05 09:01:08 +0200

    Create minimal CLI
    Signed-off-by: Sergio Castaño Sánchez <sergiocastanosanchez@icloud.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use clap::Parser;
+
+/// A simple CLI for silo.
+#[derive(Parser)]
+#[command(name = "silo", version, about)]
+struct Cli;
+
 fn main() {
-    println!("Hello, world!");
+    Cli::parse();
 }
